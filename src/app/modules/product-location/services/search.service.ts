3b1863307276ce29f78754817b83bacaff05bad3
Jarol Cachay <ext_jcachayc@tottus.com.pe>
2022-07-18 14:13:12 -0500

    Updtae

--- src/app/modules/product-location/services/search.service.ts
+++ src/app/modules/product-location/services/search.service.ts
@@ -8,32 +8,27 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class SearchService {
-  private readonly URL_JARVIS = environment.api_jarvis;
-  private readonly URL_SPREADSHEET = environment.api_spreadsheet;
-
+  private readonly URL_EXP_DIGITAL_TDA = environment.API_EXP_DIGITAL_TDA;
   constructor(private http: HttpClient) {}
-
   searchProducts$(
     code_store: string,
     product_description: string
   ): Observable<any> {
-    const body = {
-      code_store: code_store,
-      code_hierarchy: '',
-      country: 'PE',
-      product_description: product_description,
-    };
-
     const headers = {
       'x-country': 'PE',
       'x-commerce': 'Tottus',
       'x-usrtx': 'tss',
     };
 
-    return this.http.post(
-      'https://lid-exp-dot-tot-bi-corp-chatbot-dev.uc.r.appspot.com/exp-digital-tda/checklocation',
-      body,
-      { headers }
-    );
+    const body = {
+      code_store: code_store,
+      code_hierarchy: '',
+      country: headers['x-country'],
+      product_description: product_description,
+    };
+
+    return this.http.post(this.URL_EXP_DIGITAL_TDA + '/checklocation', body, {
+      headers,
+    });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  api_jarvis : `https://www.tottus.com.pe/api/product-search?`,
-   api_spreadsheet:'https://chatbot-spreadsheet-dot-tot-bi-corp-chatbot-dev.appspot.com/api/v1/Request/FindSpreadsheetTiendaUbicacionByJerarquia'
+  API_EXP_DIGITAL_TDA:"https://lid-exp-dot-tot-bi-corp-chatbot-dev.uc.r.appspot.com/exp-digital-tda",
 };
